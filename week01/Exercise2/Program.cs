453b134e5b555a6fd393faa02fdcd740927017ed
marvinmugume106-star <marvinmugume106@gmail.com>
2026-05-13 13:00:52 +0300

    percentage

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage.");
+        string gradeInput = Console.ReadLine();
+        double grade = double.Parse(gradeInput);
+
+        string letterGrade;
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else
+        {
+            letterGrade = "C";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
     }
 }
